1b6ea7eebd972c62d3f8c174ebe9882059e091e4
babuuh <kiharajoseph72@gamil.com>
2022-03-12 07:46:54 +0300

    adding a header component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 import {Routes, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop";
-
-function App() {
-  return (
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/shop" element={<ShopPage />} />
-      </Routes>
-import './App.css';
-import HomePage from './pages/homepage/homepage';
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div>
-        <HomePage />
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
     </div>
-  );
+  )
 }
-
 export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,29 @@
+import {React} from 'react';
+import './header.styles.scss';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+const Header = () => {
+    return(
+        <div className="header">
+            <Link to='/'>
+                <div className="logo-container">
+                <Logo className='logo' />
+                </div>
+            </Link>
+            <div className="options">
+                <Link to='/shop' className='option'>
+                    SHOP
+                </Link>
+                <Link to='/shop' className='option'>
+                    CONTACT
+                </Link>
+                <Link to='/' className='option'>
+                    SIGN IN
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
